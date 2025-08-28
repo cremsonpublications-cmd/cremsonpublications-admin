5146df8cf9fef4ed8819a75846ad36da59a1b0d5
santhosh v <santhoshv@sketchbrahma.com>
2025-08-29 01:17:34 +0530

    desktop footer completed

---
+++ src/constants/footerContent.jsx
@@ -0,0 +1,99 @@
+export const addressData = {
+  title: "Address:",
+  addressContent: [
+    "Arjun Fire Works,",
+    "No. 332/2, Tiruthangal Road,",
+    "Sivakasi - 626130.",
+    "Tamil Nadu. India",
+  ],
+};
+
+export const quickLinks = {
+  title: "Quick Links",
+  link: [
+    {
+      id: "home",
+      displayName: "Home",
+      redirectionLink: "#",
+    },
+    {
+      id: "products",
+      displayName: "Products",
+      redirectionLink: "#",
+    },
+    {
+      id: "newArrivals",
+      displayName: "New Arrivals",
+      redirectionLink: "#",
+    },
+    {
+      id: "categories",
+      displayName: "Categories",
+      redirectionLink: "#",
+    },
+    {
+      id: "giftBoxes",
+      displayName: "Gift Boxes",
+      redirectionLink: "#",
+    },
+    {
+      id: "contactUs",
+      displayName: "Contact Us",
+      redirectionLink: "#",
+    },
+  ],
+};
+
+export const bankDetails = {
+  title: "Bank Details",
+  data: [
+    "Name : Arjunan S",
+    "KVB Acc.No : 14000 10000 16545",
+    "IFSC Code: KVB1029",
+    "Branch : Sivakasi",
+    "Type : Savings Account",
+  ],
+};
+
+export const helpLinks = {
+  title: "Get Help",
+  links: [
+    {
+      id: "yourOrders",
+      displayName: "Your Orders",
+      redirectionLink: "#",
+    },
+    {
+      id: "trackOrders",
+      displayName: "Track Orders",
+      redirectionLink: "#",
+    },
+    {
+      id: "Privacy Policy",
+      displayName: "Privacy Policy",
+      redirectionLink: "#",
+    },
+    {
+      id: "termsConditions",
+      displayName: "Terms & Conditions",
+      redirectionLink: "#",
+    },
+  ],
+};
+
+export const footerContact = [
+  {
+    id: "mobile",
+    title: "Mobile:",
+    content: " +91 94890 23222",
+  },
+  {
+    id: "personnal",
+    title: "Office:",
+    content: " +91 94890 22222",
+  },
+];
+export const emailContact = {
+  title: "Email:",
+  content: "arjunfireworks@gmail.com",
+};
