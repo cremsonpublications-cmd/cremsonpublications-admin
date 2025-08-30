bf40a945ec7922a48c103e8d6d85f2f2e1fd448b
santhosh v <santhoshv@sketchbrahma.com>
2025-08-31 00:46:42 +0530

    product section view more yet to add

--- src/components/UiCompnents/CommonButton/CommonButton.jsx
+++ src/components/UiCompnents/CommonButton/CommonButton.jsx
@@ -1,7 +1,7 @@
 import "./commonButton.scss";
 
-const CommonButton = ({ buttonText = "Buy Now", styleClass = "orangeXl" }) => {
-  return <button className={styleClass}>{buttonText}</button>;
+const CommonButton = ({ buttonText = "Buy Now", styleClass = "orangeXl",onClick }) => {
+  return <button className={styleClass} onClick={onClick}>{buttonText}</button>;
 };
 
 export default CommonButton;
---
+++ src/components/UiCompnents/NewBadge/NewBadge.jsx
@@ -0,0 +1,10 @@
+import "./newBadge.scss"
+const NewBadge = () => {
+  return (
+    <span className="newBadge">
+      New
+    </span>
+  )
+}
+
+export default NewBadge
