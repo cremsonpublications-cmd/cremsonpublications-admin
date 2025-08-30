86cf3a3ab9a15ecb9376d37cecda7cf778d97768
santhosh v <santhoshv@sketchbrahma.com>
2025-08-30 17:11:27 +0530

    gift box carousel completed

--- src/components/GiftBoxCarousel/GiftBoxCarousel.jsx
+++ src/components/GiftBoxCarousel/GiftBoxCarousel.jsx
@@ -1,4 +1,5 @@
 import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 import { giftBoxCaroselContents } from "../../constants/giftBoxCaroselContents/GiftBoxCaroselContents";
 
@@ -6,12 +7,50 @@ import greenTick from "../../assets/imageComponents/greenTick.svg";
 
 import "./giftBoxCarousel.scss";
 import CommonButton from "../UiCompnents/CommonButton/CommonButton";
+import ArrowIcon from "../SvgComponents/ArrowIcon/ArrowIcon";
 const GiftBoxCarousel = () => {
   return (
     <>
-      <section className="giftBoxyCarousel">
-        <div className="container giftBoxyCarouselContainer">
-          
+      <section className="giftBoxCarousel">
+        <div className="container giftBoxCarouselContainer">
+          <Carousel
+            showThumbs={false} // Hide thumbnails
+            showStatus={false} // Hide status (e.g., 1/3)
+            infiniteLoop={true} // Infinite looping
+            autoPlay={true} // Auto slide
+            interval={3000} // 3 seconds per slide
+            stopOnHover={true} // Pause on hover
+            swipeable={true} // Enable swipe for touch
+            emulateTouch={true} // Mobile touch behavior
+            className="giftBoxCarousel"
+            renderArrowPrev={(onClickHandler, hasPrev, label) =>
+              hasPrev && (
+                <button
+                  type="button"
+                  onClick={onClickHandler}
+                  className="customArrow customArrowPrev"
+                  aria-label={label}
+                >
+                  <ArrowIcon fillColor="#fff" arrowColor="#FF6B00"/>
+                </button>
+              )
+            }
+            renderArrowNext={(onClickHandler, hasNext, label) =>
+            hasNext && (
+              <button
+                type="button"
+                onClick={onClickHandler}
+                className="customArrow customArrowNext"
+                aria-label={label}
+              >
+                {/* Rotate your icon for next arrow */}
+                <div style={{ transform: "rotate(180deg)" }}>
+                  <ArrowIcon fillColor="#FF6B00" arrowColor="#fff"/>
+                </div>
+              </button>
+            )
+          }
+          >
             {giftBoxCaroselContents.map((item) => (
               <div className="giftBoxSildeContainer">
                 <div className="imageSection">
@@ -49,6 +88,7 @@ const GiftBoxCarousel = () => {
                 </div>
               </div>
             ))}
+          </Carousel>
         </div>
       </section>
     </>
